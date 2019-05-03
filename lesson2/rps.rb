18e7ec6e656c447489c2e6a9eadaa4dcda7bcddb
LR <lr177245@gmail.com>
2019-05-02 20:20:16 -0700

    worked on RPS more

--- lesson2/rps.rb
+++ lesson2/rps.rb
@@ -5,28 +5,31 @@ def prompt(message)
   print "> #{message}"
 end
 
+# prompt player to choose from OPTIONS
 def player_rps
   loop do
-    prompt("YOU: choose #{OPTIONS.join(', ')}. ")
+    prompt("Choose #{OPTIONS.join(', ')}: ")
     choice = gets.chomp.downcase
     break choice unless OPTIONS.include?(choice) == false
   end
 end
 
+# generate computer's choice
 def comp_rps
   OPTIONS.sample
 end
 
+# decide the winner based on player's and
+# computer's choice
 def winner(player, comp)
-  return 't' if player == comp
+  return 'tie' if player == comp
   if player == 'rock'
-    comp == 'scissors' ? result = 'p' : result = 'c'
+    comp == 'scissors' ? 'player' : 'comp'
   elsif player == 'paper'
-    comp == 'rock' ? result = 'p' : result = 'c'
+    comp == 'rock' ? 'player' : 'comp'
   else # player = scissors
-    comp == 'rock' ? result = 'c' : result = 'p'
+    comp == 'rock' ? 'comp' : 'player'
   end
-  result
 end
 
 system "clear"
@@ -34,19 +37,18 @@ system "clear"
 prompt("🗿📄✂️ rock paper scissors game 🗿📄✂️\n")
 prompt("\n")
 
+# loop until player decides to stop
 loop do
   player = player_rps
   comp = comp_rps
   prompt("#{player} vs. #{comp} ... ")
   case winner(player, comp)
-  when 't'
+  when 'tie'
     print "TIE\n"
-  when 'p'
+  when 'player'
     print "You Win!\n"
-  when 'c'
+  when 'comp'
     print "Computer Wins!\n"
-  else
-    print "Something Broke!\n"
   end
   prompt("Play again? (y/n) ")
   break unless gets.chomp.downcase == 'y'
--- lesson2/test.rb
+++ lesson2/test.rb
@@ -8,3 +8,16 @@ string = 'hello'
 string2 = fix(string)
 p string
 p string2
+
+
+def test_method
+  prompt('test message')
+end
+
+test_method           # second, try invoking "test_method" here
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+test_method           # first, try invoking "test_method" here
