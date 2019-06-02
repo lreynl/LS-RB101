0678b83afef374ba1b496a0e70d18810fc9ac773
LR <lr177245@gmail.com>
2019-06-01 21:31:07 -0700

    worked on TTT

--- lesson6/ttt.rb
+++ lesson6/ttt.rb
@@ -1,6 +1,16 @@
-board = { 1 => ' ', 2 => ' ', 3 => ' ',
-          4 => ' ', 5 => ' ', 6 => ' ',
-          7 => ' ', 8 => ' ', 9 => ' ' }
+#board = { 1 => ' ', 2 => ' ', 3 => ' ',
+#          4 => ' ', 5 => ' ', 6 => ' ',
+#          7 => ' ', 8 => ' ', 9 => ' ' }
+
+def prompt(text)
+  print "> #{text}"
+end
+
+def init_board
+  board = {}
+  (1..9).each { |num| board[num] = ' ' }
+  board
+end
 
 def update_board(board, space, piece)
   board[space] = piece if board[space] == ' '
@@ -8,12 +18,26 @@ end
 
 def display_board(board)
   puts "#{board[1]}|#{board[2]}|#{board[3]}"
-  puts "------"
+  puts "-+-+-"
   puts "#{board[4]}|#{board[5]}|#{board[6]}"
-  puts "------"
+  puts "-+-+-"
   puts "#{board[7]}|#{board[8]}|#{board[9]}"
 end
-  
-update_board(board, 5, 'X')
+
+def player_move(board)
+  square = ''
+  loop do
+    prompt('Choose square 1 - 9: ')
+    square = gets.chomp.to_i
+    break if square >= 1 && square <= 9
+  end
+  update_board(board, square, 'X')
+end
+
+board = init_board
+player_move(board)
+#update_board(board, 5, 'X')
+#update_board(board, 1, 'O')
+#update_board(board, 9, 'X')
 display_board(board)
   
