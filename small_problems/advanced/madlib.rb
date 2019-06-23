8dd6fc01da9724a9ac55947751fc25000524c397
LR <lr177245@gmail.com>
2019-06-22 21:44:43 -0700

    got madlibs working

--- small_problems/advanced/madlib.rb
+++ small_problems/advanced/madlib.rb
@@ -1,19 +1,22 @@
-def words(type, list = LIST)
-  list[type].sample
-end
-
 LIST = { noun: %w(cat dog person house car bird critter tree spider caterpillar),
          adjective: %w(red orange yellow green blue purple quick slow silly weird normal suspicious ordinary),
          adverb: %w(slowly angrily quickly stupidly absentmindedly happily calmly agitatedly clearly),
          verb: %w(went walked ran said spoke climbed sat ate gobbled drank twisted hit cleaned touched) }
 
-TEXT = "The #{words(:adjective)} #{words(:adjective)} #{words(:noun)} #{words(:adverb)}\n" \
-       "#{words(:verb)} the #{words(:adjective)} #{words(:adjective)}\n" \
-       "#{words(:noun)}, who #{words(:adverb)} #{words(:verb)} its\n" \
-       "#{words(:noun)} and #{words(:verb)} #{words(:adverb)}."
+def words(type, list = LIST)
+  list[type].sample
+end
 
-def madlib(text = TEXT)
+def load_text(file_name)
+  text = ""
+  File.open(file_name).each { |line| text += (line.chomp + ' ') }
   text
 end
 
-puts madlib
+def madlib(txt)
+  eval txt
+end
+
+text = load_text("mltext.txt")
+puts text.class
+puts madlib(text)
